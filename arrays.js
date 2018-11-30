db31c02fb2040f87de9637f7733187878cac965e
nahomiyoz <nahomiyoz@gmail.com>
2018-11-30 21:19:12 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars=["sinkers","hundred grand","kitkat","skittles" ]
+function addElementToBeginningOfArray(array,name){
+  addElementToBeginningOfArray("fingers");
+  
+  
+}
+function destructivelyAddElementToBeginningOfArray([],String){
+  var destructivelyAddElementToBeginningOfArray=["great",...destructivelyAddElementToBeginningOfArray]
+}
