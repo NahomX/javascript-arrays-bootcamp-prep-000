7e872e224086e618730c6e332a948a7cca030f98
nahom W <nahomiyoz@gmail.com>
2018-12-03 22:35:48 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+
+var chocolateBars=["sinkers","hundred grand","kitkat","skittles" ]
+function addElementToBeginningOfArray(array,element){
+  chocolateBars=chocolateBars.unshift("element")
+  chocolateBars=[...element]
+  
+}
+function destructivelyAddElementToBeginningOfArray(Array,element){
+chocolateBars=[...element]  
+}
+
+function addElementToBeginingOfArray(Array,element){
+  
+}
+function destructivelyAddElementToEndOfArray(Array,element){
+  
+}
+function accessElementInArray(Array,Number){
+ console.log(chocolateBars[Number]);
+}
