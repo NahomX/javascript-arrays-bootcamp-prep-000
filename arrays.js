fd4ab7a1c9b4904c9469275f45c423305c7b733b
nahom W <nahomiyoz@gmail.com>
2018-12-03 06:24:04 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars=["sinkers","hundred grand","kitkat","skittles" ]
+function addElementToBeginningOfArray(array,element){
+  chocolateBars=[...element]
+  
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+chocolateBars=[...element]  
+}
+function addElementToEndOfArray(array,element){
+  chocolateBars=chocolateBars.push(element)
+}
